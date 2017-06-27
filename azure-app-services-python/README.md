ba9f43b80dc6ec8475ee5e634a58ce647b7818fd
Yoichi Kawasaki <yoichika@microsoft.com>
2017-06-27 22:20:35 +0900

    Added azure-app-services-python

---
+++ azure-app-services-python/README.md
@@ -0,0 +1,4 @@
+Docker image for Azure App Services Python
+=======================
+This image is Azure App Services Python Runtime stack or applications
+
---
+++ azure-app-services-python/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x -e
+
+if [ $# -ne 3 ]
+then
+    echo "$0 [username] [dockerhub account] [dockerhub password]"
+    exit
+fi
+echo "username=$1"
+echo "docker account name=$2"
+echo "docker account passwd=$3"
+version=`cat ./simple-flask-app/VERSION`
+tag="simple-flask-app-$version"
+echo "tag=$tag"
+
+#docker build -t "$1"/app-services-python:$tag ./simple-flask-app
+docker login -u "$2" -p "$3"
+docker push "$1"/app-services-python:$tag
+
+docker logout
