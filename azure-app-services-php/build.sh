8d9e79eb275c8039dc8a906d994b1603aec661a8
Yoichi Kawasaki <yoichika@microsoft.com>
2017-06-29 06:57:24 +0900

    added build + deploy entry for relevant apps

--- azure-app-services-php/build.sh
+++ azure-app-services-php/build.sh
@@ -9,12 +9,19 @@ fi
 echo "username=$1"
 echo "docker account name=$2"
 echo "docker account passwd=$3"
-version=`cat ./5.6-apache/VERSION`
-tag="5.6-apache2-$version"
-echo "tag=$tag"
 
-docker build -t "$1"/php:$tag ./5.6-apache
 docker login -u "$2" -p "$3"
-docker push "$1"/php:$tag
+
+version=`cat ./apache2-php5.6-app/VERSION`
+tag="apache2-php5.6-app-$version"
+echo "tag=$tag"
+docker build -t "$1"/app-services-php:$tag ./apache2-php5.6-app
+docker push "$1"/app-services-php:$tag
+
+version=`cat ./apache2-php7-app/VERSION`
+tag="apache2-php7-app-$version"
+echo "tag=$tag"
+docker build -t "$1"/app-services-php:$tag ./apache2-php7-app
+docker push "$1"/app-services-php:$tag
 
 docker logout
