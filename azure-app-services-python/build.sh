77c9cd52edd331e213d02a930021eeb133fc3960
Yoichi Kawasaki <yoichika@microsoft.com>
2017-06-29 06:59:27 +0900

    added build + deploy entry for relavant apps

--- azure-app-services-python/build.sh
+++ azure-app-services-python/build.sh
@@ -9,12 +9,13 @@ fi
 echo "username=$1"
 echo "docker account name=$2"
 echo "docker account passwd=$3"
+
+docker login -u "$2" -p "$3"
+
 version=`cat ./simple-flask-app/VERSION`
 tag="simple-flask-app-$version"
 echo "tag=$tag"
-
-#docker build -t "$1"/app-services-python:$tag ./simple-flask-app
-docker login -u "$2" -p "$3"
+docker build -t "$1"/app-services-python:$tag ./simple-flask-app
 docker push "$1"/app-services-python:$tag
 
 docker logout
