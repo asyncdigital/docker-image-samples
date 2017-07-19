f5a7d429f05df61788ac1cb212311da87c2046c0
Yoichi Kawasaki <yoichika@microsoft.com>
2017-07-19 04:12:32 -0700

    added Angular sample

---
+++ azure-app-services-angular/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -x -e
+
+if [ $# -ne 3 ]
+then
+    echo "$0 [username] [dockerhub account] [dockerhub password]"
+    exit
+fi
+echo "username=$1"
+echo "docker account name=$2"
+echo "docker account passwd=$3"
+
+docker login -u "$2" -p "$3"
+
+version=`cat ./angular4-app/VERSION`
+tag="angular4-app-$version"
+echo "tag=$tag"
+docker build -t "$1"/app-services-angular:$tag ./angular4-app
+docker push "$1"/app-services-angular:$tag
+
+docker logout
